cac92f6d29c29f5b7907f5b1f702bef81d8c3b1b
HalfVizsh <muhmark246@gmail.com>
2026-02-23 01:20:17 +0700

    Basic Player Health and Movement
    Added a very basic Player Health and Movement

--- Assets/Assets/Scripts/Player/Ply_Movement.cs
+++ Assets/Assets/Scripts/Player/Ply_Movement.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player_Mov : MonoBehaviour
+public class Ply_Movement : MonoBehaviour
 {
+    [Header("Movement")]
+    public InputAction MoveButton;
+    public float speed;
+    Rigidbody2D rb2D;
+    Vector2 move;
+    [Header("Dodge")]
+    [SerializeField] Ply_Health ply_Health;
+    public InputAction DodgeButton;
+    Vector2 facing = new Vector2 (0,1);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MoveButton.Enable();
+        DodgeButton.Enable();
+        rb2D = GetComponent<Rigidbody2D>();
+        ply_Health = GetComponent<Ply_Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move = MoveButton.ReadValue<Vector2>();
+
+        if (!Mathf.Approximately(move.x, 0.0f)|| !Mathf.Approximately(move.y,0.0f))
+        {
+            facing.Set (move.x, move.y);
+            facing.Normalize();
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        Vector2 position = rb2D.position + speed * Time.deltaTime * move;
+        rb2D.MovePosition(position);
     }
 }
